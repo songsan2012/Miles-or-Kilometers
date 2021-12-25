3d8adac5458728474ae794d03acc9a18378dbe19
Song San <songsan2012@gmail.com>
2021-12-25 10:37:07 -0800

    1st Commit

--- Miles or Kilometers/ViewController.swift
+++ Miles or Kilometers/ViewController.swift
@@ -10,11 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var tenKLabel: UILabel!
+    
+    @IBOutlet weak var marathonLabel: UILabel!
+    
+    @IBOutlet weak var fiftyKLabel: UILabel!
+    
+    
+    @IBOutlet weak var useMeticStatus: UISwitch!
+    
+    let milesToKm = 1.6
+    let useMetric = true
+    
+    var tenK = 10.0
+    var marathon = 26.2
+    var fiftyK = 50.0
+    
+    // -- Notes
+    // Marathon is set in miles
+    // 10K and 50K in kilometers
+    
+    let miles = "Miles"
+    let kilometers = "Kilometers"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // -- Set title
+//        if useMetric == true
+//        {
+//            titleLabel.text = kilometers
+//        }
+//        else
+//        {
+//            titleLabel.text = miles
+//        }
+        
+        checkMetric()
+        
     }
+    
+    
+    @IBAction func useMetricChanged(_ sender: Any) {
+        
+        // -- Check status of metric
+        
+        print("Use Metric changed")
+        
+//        if useMetric == false
+//        {
+//            titleLabel.text = "
+//        }
+        
+        
+    }
+    
+    func checkMetric()
+    {
+        if useMetric == true
+        {
+            titleLabel.text = kilometers
+        }
+        else
+        {
+            titleLabel.text = miles
+        }
+        
+    }
+    
 
-
+    
+    
 }
 
